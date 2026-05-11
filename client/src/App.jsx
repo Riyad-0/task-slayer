5d163a80404bb19456694e3f96a98f7bd7e395aa
Riyad-0 <riyad.rahman73@myhunter.cuny.edu>
2026-05-10 22:50:24 -0400

    c

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,20 @@ import ClassPage from "./ClassPage";
 import Home from './Home';
 import './global.css';
 import Test from './Test';
-import { createContext } from 'react';
+import { generateSessionId, GuestIdContext } from './GuestIdContext';
+import { useState } from 'react';
 
 function InsideRouter() {
+  const [guestId, setGuestId] = useState(generateSessionId());
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/class" element={<ClassPage />} />
-        <Route path="/test" element={<Test />} />
-      </Routes>
+      <GuestIdContext value={guestId}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/class" element={<ClassPage />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </GuestIdContext>
     </>
   );
 }
--- client/src/GuestIdContext.js
+++ client/src/GuestIdContext.js
@@ -1,11 +1,11 @@
 import { createContext } from "react";
 
-const GuestContext = createContext(generateSessionId());
+export const GuestIdContext = createContext(/** @type {string | null} */ (null));
 
 /**
  * 
  * @returns {string}
  */
-function generateSessionId() {
+export function generateSessionId() {
   return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString();
 }
